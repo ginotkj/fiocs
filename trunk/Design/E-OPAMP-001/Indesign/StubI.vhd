1edef4629982a522c52ede7ae9a6ec64e1800a7f
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2009-04-12 23:17:40 +0000

    Proyecto nuevo: E-OPAMP-001

---
+++ trunk/Design/E-OPAMP-001/Indesign/StubI.vhd
@@ -0,0 +1,4 @@
+-- Generated PORTMAP Stub File: Created by Capture FPGA Flow
+-- Matches PCB component pinout with simulation model
+-- Created Sunday, April 12, 2009 20:05:17 Argentina Standard Time
+
