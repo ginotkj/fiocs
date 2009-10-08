c58cb116bede2482f6a44753ed404edbd94f284c
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2009-10-08 02:15:29 +0000

    todos los bloques completados falta terminar esquematico de prueba

--- trunk/Design/I-DECODER-001/simul.cmd
+++ trunk/Design/I-DECODER-001/simul.cmd
@@ -1,3 +1,15 @@
-.OP
-.TRAN 1ns 1us
-.PLOT TRAN v(vout)
+.control
+set hcopydevtype=postscript
+set hcopypscolor=true
+set hcopywidth=1000
+set hcopyheight=600
+
+op
+destroy all
+
+tran 1ns 500us
+
+hardcopy vout.ps v(vout) v(vin)
+
+* PLOT TRAN v(vout)
+.endc
