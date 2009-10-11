b1402342f1627aba7fb0d0af2c88eceee1ffd999
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2009-10-11 04:26:18 +0000

    circuito de pruebas andando falta chequear las salidas de los componentes

--- trunk/Design/I-DECODER-001/simul.cmd
+++ trunk/Design/I-DECODER-001/simul.cmd
@@ -7,9 +7,12 @@ set hcopyheight=600
 op
 destroy all
 
-tran 1ns 500us
+tran 100ns 1.3ms 0
+* tran 1ns 5us 0
 
-hardcopy vout.ps v(vout) v(vin)
+hardcopy vins.ps v(v2) v(v3) v(v4) v(v5) v(v6) v(v7) v(v8) ylimit 0 7 xlimit 0 1.3m xlabel time ylabel volts title INPUTS
+* hardcopy vins.ps v(v2)
+hardcopy vout.ps v(out1) v(out2) v(out3) v(out4) v(out5)
 
 * PLOT TRAN v(vout)
 .endc
