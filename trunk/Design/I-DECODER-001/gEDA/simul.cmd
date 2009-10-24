bde7f56fc2bd7bfdebb230875faf05dd83e16038
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2009-10-24 01:30:44 +0000

    decodificador andando! falta tunear para mayor velocidad!!

--- trunk/Design/I-DECODER-001/gEDA/simul.cmd
+++ trunk/Design/I-DECODER-001/gEDA/simul.cmd
@@ -1,18 +1,19 @@
 .control
 set hcopydevtype=postscript
 set hcopypscolor=true
-set hcopywidth=1000
-set hcopyheight=600
+set hcopywidth=1200
+set hcopyheight=500
+set filetype=binary
+set combplot
 
-op
-destroy all
+* op
+* destroy all
 
-tran 1ns 1.3ms 0
-* tran 1ns 5us 0
+* tran 100ns 800us 0
+tran 10ns 320us 0
 
-hardcopy vins.ps v(v2) v(v3) v(v4) v(v5) v(v6) v(v7) v(v8) ylimit 0 7 xlimit 0 1.3m xlabel time ylabel volts title INPUTS
-* hardcopy vins.ps v(v2)
-hardcopy vout.ps v(out1) v(out2) v(out3) v(out4) v(out5)
+* hardcopy vins.ps v(v2) v(v3) v(v4) v(v5) v(v6) v(v7) v(v8) ylimit 0 7 xlimit 0 1.3m xlabel time ylabel volts title INPUTS
+* hardcopy vout.ps v(out1) v(out2) v(out3) v(out4) v(out5)
 
-* PLOT TRAN v(vout)
+write i-decoder.bin
 .endc
