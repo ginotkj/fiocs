e62d1bc1080e6bdc67089a1738001bc6f434c575
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2009-11-13 01:09:22 +0000

    algunos glichts en el flash

--- Design/I-FLASH-01/gEDA/simul.cmd
+++ Design/I-FLASH-01/gEDA/simul.cmd
@@ -10,7 +10,7 @@ set combplot
 * destroy all
 
 * tran 100ns 800us 0
-tran 10ns 340us 0
+tran 10ns 140us 0
 
 * hardcopy vins.ps v(v2) v(v3) v(v4) v(v5) v(v6) v(v7) v(v8) ylimit 0 7 xlimit 0 1.3m xlabel time ylabel volts title INPUTS
 * hardcopy vout.ps v(out1) v(out2) v(out3) v(out4) v(out5)
