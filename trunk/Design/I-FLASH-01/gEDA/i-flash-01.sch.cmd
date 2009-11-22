3090d4010b94d7a19dbc2dcd8dee109fcb8a6c64
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2009-11-22 23:20:58 +0000

    ajustando algunos parametros

--- trunk/Design/I-FLASH-01/gEDA/i-flash-01.sch.cmd
+++ trunk/Design/I-FLASH-01/gEDA/i-flash-01.sch.cmd
@@ -10,9 +10,10 @@ set combplot
 * destroy all
 
 * tran 100ns 800us 0
-*tran 20ns 500us 0
+tran 20ns 500us 0
+
 * test for make
-tran 20ns 1us
+*tran 20ns 10us 0
 
 * hardcopy vins.ps v(v2) v(v3) v(v4) v(v5) v(v6) v(v7) v(v8) ylimit 0 7 xlimit 0 1.3m xlabel time ylabel volts title INPUTS
 * hardcopy vout.ps v(out1) v(out2) v(out3) v(out4) v(out5)
