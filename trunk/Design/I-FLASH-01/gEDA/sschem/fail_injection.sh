c84c57c19268cc823b724c060bd6bde607ec8d7d
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2009-12-16 00:14:40 +0000

    script que automatiza

---
+++ trunk/Design/I-FLASH-01/gEDA/sschem/fail_injection.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+##
+## fail_injection.sh
+## 
+## Made by Facundo
+## Login   <facundo@faku-laptop>
+## 
+## Started on  Tue Dec 15 20:47:38 2009 Facundo
+## Last update Tue Dec 15 20:47:45 2009 Facundo
+##
+
