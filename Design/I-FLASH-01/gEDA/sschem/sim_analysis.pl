a949ce1f5c4a8e6b75e7b6536ed0809fc814f3d9
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2010-01-30 23:59:04 +0000

    analisis de resultados PoC andando

--- Design/I-FLASH-01/gEDA/sschem/sim_analysis.pl
+++ Design/I-FLASH-01/gEDA/sschem/sim_analysis.pl
@@ -9,40 +9,134 @@
 #
 #
 
-$file="/tmp/tmp.Tn4cx506YC/flash.bin";
+use strict;
+use warnings;
+
+my $file="$ARGV[0]/flash.bin";
 
 open FILE, "<", $file or die $!;
-
 my @lines = <FILE>;
+close(FILE);
 
 my @num_vars = grep(/No. Variables:/, @lines);
-@num_vars = split(":",@num_vars[0]);
-@num_vars = split(" ",@num_vars[1]);
+@num_vars = split(":",$num_vars[0]);
+@num_vars = split(" ",$num_vars[1]);
 my $num_vars = $num_vars[0];
 
 
 my @num_points = grep(/No. Points:/, @lines);
-@num_points = split(":",@num_points[0]);
-@num_points = split(" ",@num_points[1]);
+@num_points = split(":",$num_points[0]);
+@num_points = split(" ",$num_points[1]);
 my $num_points = $num_points[0];
 
 my @bits = grep(/lsb/, @lines);
-@bits = split(" ",@bits[0]);
+@bits = split(" ",$bits[0]);
 my $bit_0 = $bits[0];
 
 @bits = grep(/ssb/, @lines);
-@bits = split(" ",@bits[0]);
+@bits = split(" ",$bits[0]);
 my $bit_1 = $bits[0];
 
 @bits = grep(/msb/, @lines);
-@bits = split(" ",@bits[0]);
+@bits = split(" ",$bits[0]);
 my $bit_2 = $bits[0];
 
-print "bit_0: $bit_0\n";
-print "bit_1: $bit_1\n";
-print "bit_2: $bit_2\n";
+#print "num_vars: $num_vars\n";
+#print "num_points: $num_points\n";
+#print "bit_0: $bit_0\n";
+#print "bit_1: $bit_1\n";
+#print "bit_2: $bit_2\n";
 
-##
+
+my $values_found = 0; #es para cortar el archivo desde Values para arriba
+my @data;
+my $j = 0;
+
+for (my $i = 0; $i <= $#lines ; $i++) {
+    if ($lines[$i] =~ /Values:/) {
+	$values_found = 1;
+    }
+    if ($values_found == 1) {
+	unless ($lines[$i+1]) { last; }
+	$data[$j] = $lines[$i+1];
+	$j++;
+    }
+}
+
+map(s/^ (\d+)\t(\d)/$1=$2/, @data);
+
+my @sort_data = ();
+
+for (my $i = 0 ; $i < $num_points ; $i++) {
+    my @aux = ();
+    for (my $j = 0 ; $j < $num_vars ; $j++) {
+	$aux[$j] = shift(@data);
+    }
+    $sort_data[$i] = (\@aux);
+}
+
+my $global_success_sim = 0;
+my $global_failed_sim = 0;
+
+for (my $i = 0 ; $i < $#sort_data ; $i++) {
+
+    $global_success_sim = 0;
+    $global_failed_sim = 0;
+
+    if ($sort_data[$i][$bit_0] > 2) { #MSB
+	$global_success_sim++;
+    } elsif ($sort_data[$i][$bit_1] > 0) { #SSB
+	$global_success_sim++;
+    } elsif ($sort_data[$i][$bit_2] > 0) { #LSB
+	$global_success_sim++;
+    } else {
+	$global_failed_sim++;
+    }
+}
+
+if ($global_success_sim gt 0){  ## calcular todos los success que tiene que dar para estar correcto!
+    print "0";
+} elsif ($global_failed_sim gt 1){
+    print "1";
+} else {
+    print "7";
+}
+
+exit (1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#########################################################
+
+#print "DATA POINTS: $#sort_data\n";
+
+#foreach my $line (@sort_data) {
+#    my @aux = ();
+#    @aux = @{$line};
+#    print "DATA VARS: $#aux\n"
+#}
+
+########################################################
+
+#map(s/(\d+$)/$1:/, @data);
+#print "@data\n";
+
+#my @porq = grep(/^ \d+\t\d*/, @data);
+#print @porq;
+
+
+#################################################################
 ## Ver como este script interactua con el fail_injection y que cosas se aplican en cada uno
 ##
 
@@ -53,6 +147,4 @@ print "bit_2: $bit_2\n";
 # guardar en alguna varibale el nombre del cir_xxx.cir para su posterior resimulacion en formato bin para graficar
 # al finalizar todo armar tabla, zipear todo y comitear!
 
-
-close(FILE);
 
