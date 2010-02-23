7007a1f4fcd78c1751155a204aec4ec55e938668
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2010-02-23 03:10:54 +0000

    sources de los comparadores

--- Design/I-FLASH-01/gEDA/6BITS/schem/sources/gates/make_parts.sh
+++ Design/I-FLASH-01/gEDA/6BITS/schem/sources/gates/make_parts.sh
@@ -6,10 +6,10 @@
 ## Login   <facundo@faku-laptop>
 ## 
 ## Started on  Mon Feb 22 23:58:45 2010 Facundo
-## Last update Tue Feb 23 00:00:17 2010 Facundo
+## Last update Tue Feb 23 00:06:07 2010 Facundo
 ##
 
 for i in $(seq 1 1 63)
 do 
-    cp -v $1 inv.$i.sch
+    cp -v $1 $2.$i.sch
 done
