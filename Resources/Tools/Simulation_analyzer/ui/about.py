2bf70d7d221884b56759d12cf2138f3a22880c2c
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2010-04-22 03:44:52 +0000

    seleccion de files manualmente

--- Resources/Tools/Simulation_analyzer/ui/about.py
+++ Resources/Tools/Simulation_analyzer/ui/about.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'about.ui'
 #
-# Created: Wed Apr 21 19:57:04 2010
+# Created: Thu Apr 22 00:09:16 2010
 #      by: PyQt4 UI code generator 4.7.2
 #
 # WARNING! All changes made in this file will be lost!
