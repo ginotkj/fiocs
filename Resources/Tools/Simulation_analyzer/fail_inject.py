7cf2761ed1ce482272e4a1dac78fc75fccb11028
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2010-04-27 14:21:24 +0000

    related nodes bug corrected

--- Resources/Tools/Simulation_analyzer/fail_inject.py
+++ Resources/Tools/Simulation_analyzer/fail_inject.py
@@ -45,20 +45,26 @@
 #Python imports
 import sys
 import re
+import os
 
 class FailInject():
     """ This class is to manage fail injections in CIR files. """
     def __init__ (self):
         """ Initializer """
-        try:
-            self._file = sys.argv[1]
-        except IndexError, ex:
-            sys.exit("Please insert a CIR file!")
+        # Dict containing all transistors in the cir and all nodes information
         self._trans = {}
+        # Dict with node information. This var is stored for each key in _trans
         self._nodes = {}
+        # This var stores the source circuit file
         self._cirfile = ''
+        # This var has all nodes involved in a fail injection point
+        self._related_transistors = []
+        # This var contains the string expresion of the fail
+        self._fail = ''
+        # This var holds the output dir
+        self._outdir = ''
 
-    def transistor_count (self,file):
+    def _transistor_count (self,file):
         """ This method count the transistors on the given file. """
 
         f = open(file)
@@ -99,12 +105,75 @@ class FailInject():
         # close file
         pass
 
-    def run (self):
+    def _get_related_transistors(self,drainmos=None):
+        """ This method returns a list with all transistors related to a node"""
+        # Clean the list
+        self._related_transistors = []
+        if drainmos == None:
+            return None
+        for mos in self._trans:
+            nodes = self._trans[mos]["drain"] + self._trans[mos]["gate"] + \
+                    self._trans[mos]["source"] + self._trans[mos]["bulk"]
+            if drainmos in nodes:
+                self._related_transistors.append(str(mos))
+
+    def run (self,dir_=None,file_=None,fail_=None):
         """ Main method """
+        # Check if a file was received in the calling of the function
+        if file_ == None:
+            try:
+                self._file = sys.argv[1]
+                try:
+                    f = open(self._file)
+                    f = None
+                except IOError, ex:
+                    print "The file could not be open."
+                    print "ERROR: %s" % ex
+            except IndexError, ex:
+                sys.exit("Please insert a CIR file!")
+        else:
+            try:
+                f = open(file_)
+                self._file = file_
+                f = None
+            except IOError, ex:
+                print "Could not open file: %s" % file_
+                print "ERROR: %s" % ex
+
+        # Checks if an output dir was passed in the calling
+        if dir_ == None:
+            try:
+                self._outdir = os.getcwd()
+            except Exception, ex:
+                print "Error getting current directory"
+                print "ERROR: %s" % ex
+        else:
+            try:
+                tmp = os.getcwd()
+                os.chdir(dir_) # expect NameError exceptio if dir_ doesn't exist
+                self._outdir = dir_
+            except NameError, ex:
+                print "Could not change to dir %s" % dir_
+                print "ERROR: %s" % ex
+
+
         # Read the transistors to parse
-        print "Transistors: %s" % self.transistor_count(self._file)
-        # Parse file to allow fail injection
+        print "Transistors: %s" % self._transistor_count(self._file)
+        print "Fail: %s" % fail_
+        # Parse file to allow fail injection and sets: self._trans
         self._get_nodes(self._file)
+
+        # Inject the fail into the new file
+        for mos in self._trans:
+            drain_node = self._trans[mos]["drain"]
+            self._get_related_transistors(drain_node)
+            print "Transistor: %s" % mos
+            print "Drain: %s" % drain_node
+            print "Related transistors: %s" % self._related_transistors
+            print "==============================================================="
+            self._inject_fail(self._fail,self._related_transistors)
+
+        # Print a specific transistor information
         print self._trans['M_C_F_D_U99_M4']
 
 if __name__ == '__main__':
--- Resources/Tools/Simulation_analyzer/ui/testeo.py
+++ Resources/Tools/Simulation_analyzer/ui/testeo.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'testeo.ui'
 #
-# Created: Sat Apr 24 14:58:14 2010
+# Created: Sun Apr 25 16:29:19 2010
 #      by: PyQt4 UI code generator 4.7.2
 #
 # WARNING! All changes made in this file will be lost!
