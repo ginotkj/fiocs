0480a21f6204939c34069a3f8f412507cd441d9a
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2010-05-24 02:09:48 +0000

    todo funcionando teorico hacer testeo

--- trunk/Resources/Tools/Simulation_analyzer/ui/about.py
+++ trunk/Resources/Tools/Simulation_analyzer/ui/about.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'about.ui'
 #
-# Created: Tue May 18 23:30:37 2010
+# Created: Sun May 23 23:09:21 2010
 #      by: PyQt4 UI code generator 4.7.2
 #
 # WARNING! All changes made in this file will be lost!
