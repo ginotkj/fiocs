5f44cf06f8c3b5b36094567ba7182e88d6e50410
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2010-06-03 23:42:53 +0000

    para la simulacion automatica

--- Nightly-SIM/nightly-sim.cmd
+++ Nightly-SIM/nightly-sim.cmd
@@ -1,3 +1,12 @@
+
+
+REM STEP 1: Remove the files in the cir folder
+REM STEP 2: Make a SVN update for CIR folder
+REM STEP 3: Read all the files to simulate
+REM STEP 4: Simulate the files & add it to a 7z file in CSD folder
+REM STEP 5: Commit the files
+
+
 set PSPBASE=C:\OrCAD\OrCAD_16.0\tools\pspice
 set CIRBASE=C:\Documents\TESIS\fiocs\Nightly-SIM\CIR-files
 set CSDBASE=C:\Documents\TESIS\fiocs\Nightly-SIM\CSD-files
