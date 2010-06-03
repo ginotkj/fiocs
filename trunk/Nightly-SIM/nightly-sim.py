c2f27ee0d7284db8675132e26e471aa5a69887de
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2010-06-03 23:42:53 +0000

    para la simulacion automatica

---
+++ trunk/Nightly-SIM/nightly-sim.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+#
+
+#       sin título.py
+
+#
+
+#       Copyright 2010 Facundo-std <Facundo-std@FACUNDO-XPS>
+
+#
+
+#       This program is free software; you can redistribute it and/or modify
+
+#       it under the terms of the GNU General Public License as published by
+
+#       the Free Software Foundation; either version 2 of the License, or
+
+#       (at your option) any later version.
+
+#
+
+#       This program is distributed in the hope that it will be useful,
+
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+#       GNU General Public License for more details.
+
+#
+
+#       You should have received a copy of the GNU General Public License
+
+#       along with this program; if not, write to the Free Software
+
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+
+#       MA 02110-1301, USA.
+
+
+
+
+# C:\Program Files\TortoiseSVN\bin>TortoiseProc.exe
+#   /command:update /path:"D:\Documents\TESIS\fiocs\Nightly-SIM" /closeonend:1
+
+
+def main():
+
+
+
+    return 0
+
+
+def
+
+if __name__ == '__main__':
+
+    main()
+
