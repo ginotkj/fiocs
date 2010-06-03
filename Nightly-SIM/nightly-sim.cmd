0cd5cd5471513d147a42979230e600b9c33c2f82
facundo.j.ferrer <facundo.j.ferrer@01ef50be-077d-11de-b753-7319d013a8fa>
2010-06-03 01:53:46 +0000

    updated

--- Nightly-SIM/nightly-sim.cmd
+++ Nightly-SIM/nightly-sim.cmd
@@ -1,1 +1,8 @@
-dir c:\
+set PSPBASE=C:\OrCAD\OrCAD_16.0\tools\pspice
+set CIRBASE=C:\Documents\TESIS\fiocs\Nightly-SIM\CIR-files
+set CSDBASE=C:\Documents\TESIS\fiocs\Nightly-SIM\CSD-files
+set ZIPBASE=
+
+%PSPBASE%\psp_cmd.exe %CIRBASE%\*.*
+
+%ZIPBASE% %CSDBASE%\%DATE%\
